5bf3aa25893107b48dee9ad6a75df48f4afd85eb
mhonarvar18 <mhonarvar18@gmail.com>
2022-04-07 10:00:00 +0430

    you doing greate

--- main.go
+++ main.go
@@ -4,4 +4,24 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
+	foo()
+	fmt.Println("something else")
+	
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("Im in foo")
+	count := 4
+	for i := 0; i < count; i++ {
+		fmt.Println(i)
+	}
+}
+
+func bar() {
+	fmt.Println("and then we exited")
 }
